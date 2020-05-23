3f634e62cc8d0b68518ffc09b1f5291db46110d4
Obinna Daniel <obinnadaniel30@yahoo.com>
2020-05-23 12:27:50 +0100

    initial commit

---
+++ ClassesAndMethods/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ ClassesAndMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.title = "Things Fall Apart";
+            book1.author = "Chinua Achebe";
+            book1.pages = 420;
+
+            Book book2 = new Book();
+            book2.title = "Angels And Demon";
+            book2.author = "Dan Brown";
+            book2.pages = 840;
+
+            Console.WriteLine(book2.title);
+
+            Console.ReadLine();
+        }
+    }
+}
